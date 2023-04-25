bb129a8f0130e0b95845fefeb1089d84ef175212
ben lepsch <blepsch@gmail.com>
2023-04-25 17:25:54 -0400

    limit data sent to 30 times/sec

--- static/script.js
+++ static/script.js
@@ -23,7 +23,34 @@ socket.on('boo', (msg) => {
 
 });
 
+let mx = 0;
+let my = 0;
+
 document.onmousemove = (event) => {
     // console.log('mx: ' + event.pageX + '\tmy: ' + event.pageY);
-    socket.emit('new data', { 'x': event.pageX, 'y': event.pageY })
+    mx = event.pageX;
+    my = event.pageY;
 }
+
+let fpsInterval, startTime, then;
+
+function start(fps) {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+    run();
+}
+
+function run() {
+    now = Date.now();
+    elapsed = now - then;
+
+    if (elapsed > fpsInterval) {
+        then = now - (elapsed % fpsInterval);
+        socket.emit('new data', { 'x': mx, 'y': my })
+    }
+    
+    requestAnimationFrame(run);
+}
+
+start(30);
